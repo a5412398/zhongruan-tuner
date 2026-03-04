735bd80f654f9272acb99e2f1b124cedcd63859f
a5412398 <a5412398@users.noreply.github.com>
2026-03-04 21:05:17 +0800

    feat: 修改弦按钮显示为第X弦格式

--- app/src/main/java/com/lobsterai/zhongruan_tuner/ui/components/StringSelector.kt
+++ app/src/main/java/com/lobsterai/zhongruan_tuner/ui/components/StringSelector.kt
@@ -1,4 +1,4 @@
-package com.lobsterai.zhongruan_tuner.ui.components
+﻿package com.lobsterai.zhongruan_tuner.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
